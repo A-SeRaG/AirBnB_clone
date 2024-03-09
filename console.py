78f6b63d1ad29ffb24c407fe94c9263989d5b6dc
SeRaG <aserag0101@gmail.com>
2024-03-09 13:33:07 -0800

    programming

--- console.py
+++ console.py
@@ -10,17 +10,17 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb)"
 
-def do_quit(self, arg):
-    """Quit command to exit the program"""
-    return True
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
-def do_EOF(self, arg):
-    """Exit the program"""
-    print("")
-    return True
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print("")
+        return True
 
-def help_quit(self, arg):
-    print("Quit command to exit the program")
+    def help_quit(self, arg):
+        print("Quit command to exit the program")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
