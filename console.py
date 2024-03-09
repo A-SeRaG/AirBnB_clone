99e34341b3838568239f8b47d950297e63aba717
SeRaG <aserag0101@gmail.com>
2024-03-09 13:28:27 -0800

    programming

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""program called console.py that contains the entry point of the command interpreter."""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Defines the HBNB Commands
+    """
+    prompt = "(hbnb)"
+
+def do_quit(self, arg):
+    """Quit command to exit the program"""
+    return True
+
+def do_EOF(self, arg):
+    """Exit the program"""
+    print("")
+    return True
+
+def help_quit(self, arg):
+    print("Quit command to exit the program")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
