b1f150756195dbf7099668b486435d809f8df7fc
tamerkat <mahad10taha@gmail.com>
2024-03-10 11:58:49 +0200

    pash

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,77 @@
 #!/usr/bin/python3
-"""import module"""
-import uuid
 from datetime import datetime
+from uuid import uuid4
 import models
 
+"""
+Module BaseModel
+Parent of all classes
+"""
 
-class BaseModel:
-    def __init__(self, *args, **kwarg):
-        """Initialize a new BaseModel"""
-        t_form = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwarg:
-            for i, j in kwarg.items():
-                if i == "__class__":
-                    continue
-                elif i == "created_at" or i == "updated_at":
-                    setattr(self, i, datetime.strptime(j, t_form))
+
+class BaseModel():
+    """Base class for Airbnb clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        __save(self)
+        __repr__(self)
+        to_dict(self)
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize attributes: random uuid, dates created/updated
+
+
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
                 else:
-                    setattr(self, i, j)
+                    setattr(self, key, val)
         else:
-            self.id = str(uuid.uuid4())
-
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
+    def __str__(self):
+        """
+        Return string of info about model
+        """
+        return ('[{}] ({}) {}'.
+                format(self.__class__.__name__, self.id, self.__dict__))
+
+    def __repr__(self):
+        """
+        returns string representation
+        """
+        return (self.__str__())
+
     def save(self):
-        self.updated_at = datetime.utcnow()
+        """
+        Update instance with updated time & save to serialized file
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        self.iso_created = self.created_at.isoformat()
-        self.iso_updated = self.updated_at.isoformat()
-
-        object_dicit = self.__dict__.copy()
-        object_dicit["__class__"] = self.__class__.__name__
-        object_dicit["created_at"] = self.iso_created
-        object_dicit["updated_at"] = self.iso_updated
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-              my_model_json[key]))
+        """
+        Return dic with string formats of times; add class info to dic
+        """
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
+        return dic
