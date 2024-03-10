3f5387b571f645be420b2c3eea0bcf22b7c4b32b
SeRaG <aserag0101@gmail.com>
2024-03-09 22:13:45 -0800

    programming

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class"""
+import models
 import uuid
 from datetime import datetime
 
-"""class Basemodel"""
-
 
 class BaseModel:
-    def __init__(self):
-        self.id = uuid.uuid4()
+
+    """BaseModel class"""
+
+    def __init__(self, *args, **kwargs):
+        """.."""
+        t_form = '%Y-%m-%dT%H:%M:%S.%f'
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        if kwargs:
+            for i, j in kwargs.items():
+                if i == "created_at" or i == "updated_at":
+                    self.__dict__[i] = datetime.strptime(j, t_form)
+                else:
+                    self.__dict__[i] = j
+        else:
+            models.storage.new(self)
 
     def save(self):
+        """.."""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
+        """.."""
         self.iso_created = self.created_at.isoformat()
         self.iso_updated = self.updated_at.isoformat()
-
         object_dicit = self.__dict__.copy()
         object_dicit["__class__"] = self.__class__.__name__
         object_dicit["created_at"] = self.iso_created
         object_dicit["updated_at"] = self.iso_updated
 
     def __str__(self):
+        """.."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-              my_model_json[key]))
