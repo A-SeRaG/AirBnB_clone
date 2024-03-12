b1b1427b762b254bcc67479b6e8e529d2c49b31a
tamerkat <mahad10taha@gmail.com>
2024-03-13 00:05:05 +0200

    pash

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """save def."""
         self.updated_at = datetime.today()
         models.storage.save()
 
