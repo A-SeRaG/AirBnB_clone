c98044110147b2f9f60f95f047ac37f237cc30e1
SeRaG <aserag0101@gmail.com>
2024-03-12 02:24:44 -0700

    programming

--- models/base_model.py
+++ models/base_model.py
@@ -41,5 +41,3 @@ class BaseModel:
         """Return the print/str of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
-
-
