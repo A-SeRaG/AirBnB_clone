241a61954f6b05633137538a6010d4176d67cba7
tamerkat <mahad10taha@gmail.com>
2024-03-13 00:17:57 +0200

    pash

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class BaseModel:
 
-    """ def class attbute function"""
+    """ def class attbute func"""
 
     def __init__(self, *args, **kwargs):
         """ init def function"""
