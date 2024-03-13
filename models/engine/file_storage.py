62fac6f3fa0ef7d18e30975b84bb05578e643f37
tamerkat <mahad10taha@gmail.com>
2024-03-13 03:21:11 +0200

    pash

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -12,29 +11,24 @@ from models.review import Review
 
 class FileStorage:
 
-    """Represent an abstracted storage engine"""
 
     __file_path = "file.json"
     __obj = {}
 
     def all(self):
-        """Return the dictionary __objects."""
         return FileStorage.__obj
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
         obj_name = obj.__class__.__name__
         FileStorage.__obj["{}.{}".format(obj_name, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
         odj_1 = FileStorage.__obj
         obj_2 = {obj: odj_1[obj].to_dict() for obj in odj_1.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(obj_2, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
                 obj_2 = json.load(f)
